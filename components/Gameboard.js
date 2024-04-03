7626ecfffb2fdc09b389182b27388c483cd26fb9
Juttta <n1anju00@students.oamk.fi>
2024-04-03 23:12:07 +0300

    Boards

---
+++ components/Gameboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native-paper";
+import style from '../styles/style';
+
+export default function Gameboard(){
+    return (
+        <View>
+            <Text>
+                Gameboard here...
+            </Text>
+        </View>
+    )
+}
---
+++ components/Scoreboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native-paper";
+import style from '../styles/style';
+
+export default function Scoreboard(){
+    return (
+        <View>
+            <Text>
+                Scoreboard here...
+            </Text>
+        </View>
+    )
+}
